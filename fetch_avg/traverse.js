9980a96650cd72f2055bf40663f75c026b13042b
Aneesh Patne <aneeshpatne@gmail.com>
2025-11-12 18:03:28 +0530

    refactor: update import statements and function calls for consistency

--- fetch_avg/traverse.js
+++ fetch_avg/traverse.js
@@ -1,7 +1,7 @@
 import { readdirSync, statSync } from "fs";
 import { join } from "path";
-import kindle from "../connect.js";
-import helper from "../helper.js";
+import {exec} from "../connect.js";
+import {blackDisplay} from "../helper.js";
 
 function traverseDirectory(dirPath) {
   try {
@@ -19,12 +19,12 @@ function traverseDirectory(dirPath) {
 }
 
 async function sendImage() {
-  await helper.blackDisplay();
+  await blackDisplay();
 }
 
 async function main() {
-  const imageDir = join(process.cwd(), ".", "image");
-  traverseDirectory(imageDir);
+  // const imageDir = join(process.cwd(), ".", "image");
+  // traverseDirectory(imageDir);
   await sendImage();
 }
 
