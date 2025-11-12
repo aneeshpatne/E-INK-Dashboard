4462d1b71d8faecb0389424ff83df4df19522424
Aneesh <aneeshpatne@gmail.com>
2025-11-12 08:06:57 +0000

    Add redis as a dependency and implement Redis client in main.js

---
+++ fetch_avg/main.js
@@ -0,0 +1,11 @@
+import { createClient } from "redis";
+
+const redis = await createClient()
+  .on("error", (err) => console.log("Redis Client Error", err))
+  .connect();
+
+const value = await client.get("changes");
+
+const data = await value.json();
+
+console.log();
